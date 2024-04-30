64a6811e35c14343f986622b9a2d1ba65e5064d2
Jonathan Turner <turner@atlantissrv.com>
2024-04-30 12:27:46 -0400

    Finished project, submitted.

--- main.R
+++ main.R
@@ -1,57 +1,65 @@
-# This class is used as the driver, in which it will hold the main functions 
-# (data wise).
-
-# I will be using the readxl library to read in the excel file to allow for
-# Easier manipulation of the file. 
-
-library(readxl) # Used for reading in excel files.
-library(ggstatsplot) # Used for creating the box plot.
-library(ggstatsplot)
-library(paletteer)
-library(ggplot2)
+# Imports the libraries used. These libraries must be installed prior to running
+# the program.
+library(tidyverse)
 setwd("./") # Ensures the working directory is in the same file. 
 
-# Reads the excel file in to the dataset object.
-dataset <- read_excel("Data/recent-grads-excel.xlsx")
+# Reads the data set object.
+dataset <- read_csv("Data/recent-grads.csv")
 
 # This function below can remain commented, used for testing to ensure data is 
 # loaded correctly.
 # View(dataset)
 
-# Creates a first barplot of the dataset to show the distribution of the majors.
-barplot(table(dataset$Major_category), main="Distribution of Majors", 
-        xlab="Major Category", ylab="Frequency", col="RED",)
-
-# Finds the major with the highest unemployment rate.
-highest_unemployment_rate <- dataset[which.max(dataset$Unemployment_rate),]
-
-# Prints the major with the highest unemployment rate.
-print(paste("The major with the highest unemployment rate is: ", 
-            highest_unemployment_rate$Major))
-
-boxplot(as.numeric(dataset$Unemployment_rate), xlab="Unemployment Rate", 
-        ylab="Majors", horizontal = TRUE)
-
-
-# Creates a copy of the dataset for maniupation without altering the original 
-# set.
-filter_set <- dataset
-na.omit(filter_set$Unemployment_rate)
-
-# Converts the rate to a 
-filter_set$Unemployment_rate <- as.numeric(filter_set$Unemployment_rate)
-
-df <- data.frame(
-  college = filter_set$Major_category,
-  rate = filter_set$rate
+# Creates a dataframe object that observes the 
+modified <- data.frame(
+  college = dataset$Major_category, 
+  rate = dataset$Unemployment_rate
 )
 
-p1 <- ggbetweenstats(
-  data = df,
-  x = college,
-  y = rate,
-  pairwise.comparisons = FALSE,
-  
-)
+modified <- na.omit(modified) # Removes any NA data values
 
-p1
+# Performs an ANOVA test on the large range of values in the dataframe. . 
+anova_result <- aov(rate ~ college, data = modified)
+summary(anova_result)
+
+# Finds the first and 99th quadrant 
+q1 <- quantile(modified$rate, 0.01)
+q99 <- quantile(modified$rate, 0.99)
+
+#Creates a new dataset with a filtered to remove outliers. 
+filtered_outliers <- modified %>%
+  filter(rate >= q1 & rate <= q99)
+
+# performs the ANOVA test again but with outliers removed. 
+anova_result_filtered <- aov(rate ~ college, data=filtered_outliers)
+summary(anova_result_filtered)
+
+# Used to find the high and low of the values. 
+high_rate <- max(filtered_outliers$rate)
+low_rate <- min(filtered_outliers$rate)
+
+# Combines the repeated values into a single entry with a mean of all the values.
+combined_majors <- aggregate(rate ~ college, data = filtered_outliers, FUN=mean)
+
+# Displays an easier to see graph representing the college without repeat values.
+barplot(combined_majors$rate, 
+        names.arg = combined_majors$college, las = 2, 
+        xlab = "Major", ylab = "Unemployment Rate")
+
+# Finds the min and max indicies of the combined and filtered stats.
+min_index <- which.min(combined_majors$rate)
+max_index <- which.max(combined_majors$rate)
+
+# Prints out the results.
+cat("The lowest combined unemployment of a college is: ", 
+    combined_majors$college[min_index], 
+    " with a value of ", 
+    combined_majors$rate[min_index], "\n")
+
+cat("The highest combined unemployment of a college is: ", 
+    combined_majors$college[max_index], 
+    " with a value of ", 
+    combined_majors$rate[max_index], "\n")
+
+
+
